af2f3df09cafec185deae2cd4286e5e476094800
Muawiya <muawiya.asali@gm.com>
2024-03-28 19:09:37 +0200

    Adding image to the model

--- django_api/django_api/urls.py
+++ django_api/django_api/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from movies.views import MovieViewSet, ActionMoviesViewSet, ThrillerMoviesViewSet
+from django.conf import settings
+from django.conf.urls.static import static
 
 router = routers.DefaultRouter()
 router.register('movies', MovieViewSet, basename='movie')
@@ -27,4 +29,4 @@ router.register('thriller-movies', ThrillerMoviesViewSet, basename='thriller')
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- django_api/movies/models.py
+++ django_api/movies/models.py
@@ -7,6 +7,7 @@ class MovieData(models.Model):
     duration = models.FloatField()
     rating = models.FloatField()
     typ = models.CharField(max_length=200, default='Action')
+    image = models.ImageField(upload_to='images/', default='images/default.jpg')
 
     def __str__(self):
         return self.name
